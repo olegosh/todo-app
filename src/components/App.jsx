ab07ce77fccbccafb947c324ca74fd9d2d8fe6e8
Oleg Oshurkov <olegosh137@gmail.com>
2021-01-07 16:40:20 +0200

    Init dev branch

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import Logo from './Logo';
 
 export const App = () => {
     return (
         <div className="container">
-            <h1>ToDo App</h1>
+            <Logo />
         </div>
     );
 }
---
+++ src/components/Logo.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Logo = () => {
+    return (
+        <div>
+            <nav>
+                <div class="nav-wrapper">
+                    <span class="brand-logo p3">ToDo App</span>
+                </div>
+            </nav>
+        </div>
+    );
+}
+
+export default Logo;
